221d9a294dc66046600b2823925d8814913bd7c1
Victorina Joy Santos <santosednasian@icloud.com>
2020-03-10 19:40:05 -0500

    commit

--- lab3/exercise3/hw3.js
+++ lab3/exercise3/hw3.js
@@ -1,1 +1,27 @@
+function button1(){
+    document.getElementById("narutoBioBox").className = "show";
+    document.getElementById("narutoSkillBox").className = "hide";
+    document.getElementById("narutoGalleryBox").className = "hide";
+    document.getElementById("narutoVideoBox").className = "hide";
+}
 
+function button2(){
+    document.getElementById("narutoBioBox").className = "hide";
+    document.getElementById("narutoSkillBox").className = "show";
+    document.getElementById("narutoGalleryBox").className = "hide";
+    document.getElementById("narutoVideoBox").className = "hide";
+}
+
+function button3(){
+    document.getElementById("narutoBioBox").className = "hide";
+    document.getElementById("narutoSkillBox").className = "hide";
+    document.getElementById("narutoGalleryBox").className = "show";
+    document.getElementById("narutoVideoBox").className = "hide";
+}
+
+function button4(){
+    document.getElementById("narutoBioBox").className = "hide";
+    document.getElementById("narutoSkillBox").className = "hide";
+    document.getElementById("narutoGalleryBox").className = "hide";
+    document.getElementById("narutoVideoBox").className = "show";
+}
