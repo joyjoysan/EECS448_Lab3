f67cdb57470d14a376bdb003d1d7222f06476336
Victorina Joy Santos <santosednasian@icloud.com>
2020-03-20 03:04:41 -0500

    update

--- lab3/exercise4/hw4.js
+++ lab3/exercise4/hw4.js
@@ -1,1 +1,27 @@
+let text = document.getElementById("text");
 
+function border(){
+    let borderRedVal = document.getElementById("borderRed").value;
+    let borderBlueVal = document.getElementById("borderBlue").value;
+    let borderGreenVal = document.getElementById("borderGreen").value;
+    let borderWidthVal = document.getElementById("borderWidth").value;
+
+    console.log("test1");
+
+    if((borderRedVal >= 0 && borderRedVal < 256) &&
+       (borderBlueVal >= 0 && borderBlueVal < 256) &&
+       (borderGreenVal >= 0 && borderGreenVal < 256) &&
+       (borderWidthVal >= 0 && borderWidthVal < 101)){
+    
+        document.querySelector("#errorMessage").innerText = "YEET";
+    }
+    else{
+        document.querySelector("#errorMessage").innerText = "Error: Invalid border input(s).";
+    }
+}
+
+function background(){
+    let bgRedVal = document.getElementById("backgroundRed").value;
+    let bgBlueVal = document.getElementById("backgroundBlue").value;
+    let bgGreenVal = document.getElementById("backgroundGreen").value;
+}
